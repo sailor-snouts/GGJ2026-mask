a6cd9d5c325dd5763619b7e5a752edb6b4223f83
Ace Luke <acelukemusic@gmail.com>
2026-01-30 19:14:26 -0800

    Added StartMusic event with multiple transition options

--- Assets/WwiseData/Bank/Wwise_IDs.h
+++ Assets/WwiseData/Bank/Wwise_IDs.h
@@ -11,6 +11,45 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID STARTMUSIC = 3827058668U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSICSPEED
+        {
+            static const AkUniqueID GROUP = 1763041431U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID TEMPO1 = 2339970659U;
+                static const AkUniqueID TEMPO2 = 2339970656U;
+                static const AkUniqueID TEMPO3 = 2339970657U;
+            } // namespace STATE
+        } // namespace MUSICSPEED
+
+        namespace PLAYERLIFE
+        {
+            static const AkUniqueID GROUP = 444815956U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ALIVE = 655265632U;
+                static const AkUniqueID DEAD = 2044049779U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace PLAYERLIFE
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID TRACKSPEED = 2115070251U;
+    } // namespace GAME_PARAMETERS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
